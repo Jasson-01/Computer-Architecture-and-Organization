1ff4a7cc3aa28c57d071dd4ecbea3cdd230149e8
jasson-01 <jason.davila001@gmail.com>
2026-08-11 23:55:04 -0300

    Primer ejercicio de C

---
+++ Old/Autodidacta-Paro-General/Practica/1-Aprende C/Ejercicios-Guía-C-Básica/hola.c
@@ -0,0 +1,6 @@
+#include <stdio.h> //Directiva "#include" tiene a stdio.h para poder usar printf()
+
+int main() { // main es el punto de entrada
+    printf("Hola Orga!\n");
+    return 0; // Significa que no hay errores
+}
